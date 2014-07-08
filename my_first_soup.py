df812fc7318206a711862c85a214722c4d9d19b1
Selina Musuta <selina@codeforprogress.org>
2014-07-08 14:08:54 -0400

    added more comments

--- my_first_soup.py
+++ my_first_soup.py
@@ -6,7 +6,7 @@ import requests
 #beautiful soup documentation
 # anchor text is the thing you click on that leads to another page
 
-#url = raw_input("Enter a website to extract the URL's from: ")
+# A variable to receive user input.  url = raw_input("Enter a website to extract the URL's from: ")
 
 r  = requests.get("http://noisey.vice.com/en_ca/blog/the-number-one-reason-there-arent-more-female-djs" )
 
